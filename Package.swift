58ee939be4f48d934de842d9440133430341849c
vegopunk <241767368+dev-vegopunk@users.noreply.github.com>
2026-02-10 10:52:43 +0100

    Add dependency
    swift-composable-architecture

--- Package.swift
+++ Package.swift
@@ -4,7 +4,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-authentication",
+    name: "SwiftAuthentication",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,11 +15,17 @@ let package = Package(
             targets: ["swift-authentication"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/unit-core/swift-composable-architecture", exact: .init(1, 23, 1)),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-authentication"
+            name: "swift-authentication",
+            dependencies: [
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+            ]
         ),
         .testTarget(
             name: "swift-authenticationTests",
