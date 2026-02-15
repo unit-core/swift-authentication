3e9a3cf1bc07146a542f1ab72ca4e521e6f735db
vegopunk <241767368+dev-vegopunk@users.noreply.github.com>
2026-02-15 15:09:41 +0100

    Add snapshot tests for SignInWithEmailAndPassword

--- Tests/swift-authenticationTests/SnapshotTests/SignInWithEmailAndPasswordViewTests.swift
+++ Tests/swift-authenticationTests/SnapshotTests/SignInWithEmailAndPasswordViewTests.swift
@@ -11,18 +11,45 @@ import ComposableArchitecture
 
 @testable import swift_authentication
 
+struct SnapshotConfiguration: Sendable {
+    let name: String
+    let device: ViewImageConfig
+    
+    init(name: String, device: ViewImageConfig) {
+        self.name = name
+        self.device = device
+    }
+}
+
 @MainActor
 struct SignInWithEmailAndPasswordViewTests {
     
-    @Test
-    func testIdleState() {
+    @Test(arguments: [
+        SnapshotConfiguration(name: "iPhone13Mini", device: .iPhone13Mini),
+        SnapshotConfiguration(name: "iPhone13", device: .iPhone13),
+        SnapshotConfiguration(name: "iPhone13Pro", device: .iPhone13Pro),
+        SnapshotConfiguration(name: "iPhone13ProMax", device: .iPhone13ProMax)
+    ])
+    func testIdleState(_ argument: SnapshotConfiguration) {
         let view = SignInWithEmailAndPasswordView(
             store: StoreOf<SignInWithEmailAndPasswordReducer>.init(
                 initialState: SignInWithEmailAndPasswordReducer.State.init(),
                 reducer: { SignInWithEmailAndPasswordReducer() }
             )
         )
-        
-        assertSnapshot(of: view, as: .image(layout: .device(config: .iPhone13)))
+        assertSnapshot(
+            of: view,
+            as: .image(layout: .device(config: argument.device)),
+            named: "light.\(argument.name)",
+            testName: "idle"
+        )
+        assertSnapshot(
+            of: view
+                .background(.black)
+                .environment(\.colorScheme, .dark),
+            as: .image(layout: .device(config: argument.device)),
+            named: "dark.\(argument.name)",
+            testName: "idle"
+        )
     }
 }
